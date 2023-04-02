52652826bb897ad81c6da4b38e1eab1b93bc4d51
a-128 <128114816+a-128@users.noreply.github.com>
2023-04-02 21:25:27 +0100

    Update dd.js

--- dd.js
+++ dd.js
@@ -1,4 +1,5 @@
 (async () => {
+id2="5d8b57088be7c427874a91fd"
 await jQuery.ajax({url: "/j/i/gcr/" + 0 + "/" + 9999999999 + id2,context: id2}).done(a => {staffa=a.items})
 ig.game.O3167.wssend(ig.game.O3167.ws, "od", {rid: "6244c015ee34491c18fcfd5f", pack:["1", "1", {id: staffa, x: 1, y: 1}]});
 eval("ig.game.O3167.O4252="+ig.game.O3167.O4252.toString().replaceAll("c,d){", "c,d){if(b.m=='od'){lada=b};"))
